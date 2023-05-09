8f034bb5df449aef39da8514632886a56b996e58
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-05-10 01:10:19 +0330

    Add files via upload

---
+++ stat.js
@@ -0,0 +1,9 @@
+myObject= {"customers":[
+    {"chain":"افق کوروش","rank":0.68,"num":7},
+    {"chain":"جانبو" ,"rank":0.62,"num":5},
+    {"chain":"رفاه" ,"rank":0.85,"num":2},
+    {"chain":"وین_مارکت" ,"rank":0.4,"num":2},
+    {"chain":"وی_وان" ,"rank":0.4,"num":4},
+    {"chain":"دیلی_مارکت" ,"rank":0.58,"num":2},
+    {"chain":"هفت" ,"rank":0.42,"num":1},
+    ]};
