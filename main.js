16d49912662710e1d521ff38b2978f4de7c09b8a
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-05-26 13:28:19 +0330

    Update main.js

--- main.js
+++ main.js
@@ -215,18 +215,18 @@ var baseMap = {
     تهران:THR2
 };
 
-$('.story').on('click', function(){
-    // parse lat and lng from the divs data attribute
-    var latlng = $(this).data().point.split(',');
-    var lat = latlng[0];
-    var lng = latlng[1];
-    var zoom = 15;
+// $('.story').on('click', function(){
+//     // parse lat and lng from the divs data attribute
+//     var latlng = $(this).data().point.split(',');
+//     var lat = latlng[0];
+//     var lng = latlng[1];
+//     var zoom = 15;
 
-    // add a marker
-    var marker = L.marker([lat, lng],{}).addTo(map);
-    // set the view
-    map.setView([lat, lng], zoom);
-})
+//     // add a marker
+//     var marker = L.marker([lat, lng],{}).addTo(map);
+//     // set the view
+//     map.setView([lat, lng], zoom);
+// })
 
 
 var map = L.map('map', 
